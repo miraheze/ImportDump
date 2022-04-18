b07b3e9a423a95fe4c9136a30a7ceb9b6f3c1f4a
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-18 13:33:12 -0600

    Update importdump_request_comments.sql

--- sql/importdump_request_comments.sql
+++ sql/importdump_request_comments.sql
@@ -2,5 +2,5 @@ CREATE TABLE /*_*/importdump_request_comments (
   request_id INT NOT NULL,
   request_comment BLOB NOT NULL,
   request_comment_timestamp VARCHAR(32) NOT NULL,
-  request_comment_actor INT(10) NOT NULL
+  request_comment_user INT(10) NOT NULL
 ) /*$wgDBTableOptions*/;
