8199b6fa5a1c9358eedbc16e2fd2c766cec1d17a
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-18 13:39:11 -0600

    Update importdump_requests.sql

--- sql/importdump_requests.sql
+++ sql/importdump_requests.sql
@@ -1,7 +1,8 @@
 CREATE TABLE /*_*/importdump_requests (
   request_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  request_source TEXT DEFAULT NULL,
-  request_target TEXT DEFAULT NULL,
+  request_source VARCHAR(96) DEFAULT NULL,
+  request_target VARCHAR(96) NOT NULL,
+  request_file VARCHAR(500) DEFAULT NULL,
   request_reason TEXT DEFAULT NULL,
   request_status VARCHAR(16) DEFAULT NULL,
   request_user INT(10) NOT NULL
