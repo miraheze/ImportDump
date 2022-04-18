2e355421854aeae780ca830fe1cdd06d7a25bec0
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-18 12:45:48 -0600

    Create config.php

---
+++ .phan/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['suppress_issue_types'] = [
+	'PhanAccessMethodInternal',
+	'SecurityCheck-LikelyFalsePositive',
+];
+
+return $cfg;
