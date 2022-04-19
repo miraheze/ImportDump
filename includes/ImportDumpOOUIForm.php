9cf14fd0a4435f95f5ac060d164c3fcece845c23
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-19 17:58:42 -0600

    Create ImportDumpOOUIForm.php

---
+++ includes/ImportDumpOOUIForm.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Miraheze\ImportDump;
+
+use OOUI\FieldsetLayout;
+use OOUI\HtmlSnippet;
+use OOUI\IndexLayout;
+use OOUI\PanelLayout;
+use OOUI\TabPanelLayout;
+use OOUI\Widget;
+use OOUIHTMLForm;
+use Xml;
+
+class ImportDumpOOUIForm extends OOUIHTMLForm {
+
+	/** @var bool */
+	protected $mSubSectionBeforeFields = false;
+
+	public function wrapForm( $html ) {
+		$html = Xml::tags( 'div', [ 'id' => 'importdump' ], $html );
+
+		return parent::wrapForm( $html );
+	}
+
+	protected function wrapFieldSetSection( $legend, $section, $attributes, $isRoot ) {
+		$layout = parent::wrapFieldSetSection( $legend, $section, $attributes, $isRoot );
+
+		$layout->addClasses( [ 'importdump-fieldset-wrapper' ] );
+		$layout->removeClasses( [ 'oo-ui-panelLayout-framed' ] );
+
+		return $layout;
+	}
+
+	public function getBody() {
+		$tabPanels = [];
+		foreach ( $this->mFieldTree as $key => $val ) {
+			if ( !is_array( $val ) ) {
+				wfDebug( __METHOD__ . " encountered a field not attached to a section: '{$key}'" );
+
+				continue;
+			}
+
+			$label = $this->getLegend( $key );
+
+			$content =
+				$this->getHeaderText( $key ) .
+				$this->displaySection(
+					$val,
+					'',
+					"mw-section-{$key}-"
+				) .
+				$this->getFooterText( $key );
+
+			$tabPanels[] = new TabPanelLayout( 'mw-section-' . $key, [
+				'classes' => [ 'mw-htmlform-autoinfuse-lazy' ],
+				'label' => $label,
+				'content' => new FieldsetLayout( [
+					'classes' => [ 'importdump-section-fieldset' ],
+					'id' => "mw-section-{$key}",
+					'label' => $label,
+					'items' => [
+						new Widget( [
+							'content' => new HtmlSnippet( $content )
+						] ),
+					],
+				] ),
+				'expanded' => false,
+				'framed' => true,
+			] );
+		}
+
+		$indexLayout = new IndexLayout( [
+			'infusable' => true,
+			'expanded' => false,
+			'autoFocus' => false,
+			'classes' => [ 'importdump-tabs' ],
+		] );
+
+		$indexLayout->addTabPanels( $tabPanels );
+
+		$header = $this->formatFormHeader();
+
+		$form = new PanelLayout( [
+			'framed' => true,
+			'expanded' => false,
+			'classes' => [ 'importdump-tabs-wrapper' ],
+			'content' => $indexLayout
+		] );
+
+		return $header . $form;
+	}
+}
