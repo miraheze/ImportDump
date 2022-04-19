ec955e7670eb8b8cadb4624bb30d03ba26e4df26
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-19 13:14:37 -0600

    Update config.php

--- .phan/config.php
+++ .phan/config.php
@@ -2,18 +2,6 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
-$cfg['directory_list'] = array_merge(
-	$cfg['directory_list'], [
-		'../../extensions/CentralAuth',
-	]
-);
-
-$cfg['exclude_analysis_directory_list'] = array_merge(
-	$cfg['exclude_analysis_directory_list'], [
-		'../../extensions/CentralAuth',
-	]
-);
-
 $cfg['suppress_issue_types'] = [
 	'PhanAccessMethodInternal',
 	'SecurityCheck-LikelyFalsePositive',
