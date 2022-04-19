02e20e9e81e75b522493ed1f4f9add4a3687880b
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-19 11:41:13 -0600

    Update importdump_requests.sql

--- sql/importdump_requests.sql
+++ sql/importdump_requests.sql
@@ -1,11 +1,11 @@
 CREATE TABLE /*_*/importdump_requests (
   request_id INT AUTO_INCREMENT NOT NULL,
+  request_actor BIGINT UNSIGNED NOT NULL,
   request_timestamp VARCHAR(32) NOT NULL,
   request_source VARCHAR(96) DEFAULT NULL,
   request_target VARCHAR(96) NOT NULL,
   request_file VARCHAR(500) DEFAULT NULL,
   request_reason TEXT DEFAULT NULL,
   request_status VARCHAR(16) DEFAULT NULL,
-  request_user INT(10) NOT NULL,
   PRIMARY KEY(request_id)
 ) /*$wgDBTableOptions*/;
