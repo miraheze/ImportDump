9960d2cda0310606150c9190dfc0b390e8753fc5
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-18 18:17:07 -0600

    Create ImportDumpRequestQueuePager.php

---
+++ includes/ImportDumpRequestQueuePager.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Miraheze\ImportDump;
+
+use CentralAuthUser;
+use MediaWiki\MediaWikiServices;
+use TablePager;
+use Title;
+
+class ImportDumpRequestQueuePager extends TablePager {
+
+	/** @var string */
+	private $requester;
+
+	/** @var string */
+	private $dbname;
+
+	/** @var string */
+	private $status;
+
+	public function __construct( $page, $requester, $source, $target, $status ) {
+		parent::__construct( $page->getContext(), $page->getLinkRenderer() );
+
+		$this->mDb = MediaWikiServices::getInstance()->getDBLoadBalancerFactory()->getMainLB(
+			$page->getConfig()->get( 'ImportDumpRequestsDatabase' )
+		)->getConnectionRef( DB_REPLICA, [], $page->getConfig()->get( 'ImportDumpRequestsDatabase' ) );
+
+		$this->requester = $requester;
+		$this->status = $status;
+	}
+
+	public function getFieldNames() {
+		static $headers = null;
+
+		$headers = [
+			'request_timestamp' => 'importdump-request-label-requested-date',
+			'request_source' => 'importdump-label-source',
+			'request_target' => 'importdump-label-target',
+			'request_user' => 'importdump-request-label-requester',
+			'request_status' => 'importdump-request-label-status',
+		];
+
+		foreach ( $headers as &$msg ) {
+			$msg = $this->msg( $msg )->text();
+		}
+
+		return $headers;
+	}
+
+	public function formatValue( $name, $value ) {
+		$row = $this->mCurrentRow;
+
+		switch ( $name ) {
+			case 'request_timestamp':
+				$language = $this->getLanguage();
+
+				$formatted = $language->timeanddate( $row->request_timestamp );
+				break;
+			case 'request_source':
+				$formatted = $row->request_source;
+				break;
+			case 'request_target':
+				$formatted = $row->request_target;
+				break;
+			case 'request_user':
+				$globalUser = CentralAuthUser::newFromId( $row->request_user );
+
+				$formatted = $globalUser->getName();
+				break;
+			case 'request_status':
+				$formatted = MediaWikiServices::getInstance()->getLinkRenderer()->makeLink( Title::newFromText( "Special:ImportDumpRequestQueue/{$row->request_id}" ), $row->request_status );
+				break;
+			default:
+				$formatted = "Unable to format $name";
+				break;
+		}
+
+		return $formatted;
+	}
+
+	public function getQueryInfo() {
+		$info = [
+			'tables' => [
+				'importdump_requests',
+			],
+			'fields' => [
+				'request_id',
+				'request_timestamp',
+				'request_source',
+				'request_target',
+				'request_user',
+				'request_status',
+			],
+			'joins_conds' => [],
+		];
+
+		if ( $this->requester ) {
+			$globalUser = CentralAuthUser::newFromId( $this->requester );
+			$info['conds']['request_user'] = $globalUser->getId();
+		}
+
+		if ( $this->status && $this->status != '*' ) {
+			$info['conds']['request_status'] = $this->status;
+		} elseif ( !$this->status ) {
+			$info['conds']['request_status'] = 'pending';
+		}
+
+		return $info;
+	}
+
+	public function getDefaultSort() {
+		return 'request_id';
+	}
+
+	public function isFieldSortable( $name ) {
+		return true;
+	}
+}
