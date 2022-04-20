a3cedd2429aff66c109158d16687e299ccd0b20d
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-19 21:23:13 -0600

    Update ImportDumpRequestManager.php

--- includes/ImportDumpRequestManager.php
+++ includes/ImportDumpRequestManager.php
@@ -2,9 +2,14 @@
 
 namespace Miraheze\ImportDump;
 
+use ExtensionRegistry;
 use MediaWiki\Config\ServiceOptions;
 use MediaWiki\User\UserFactory;
 use MessageLocalizer;
+use Miraheze\CreateWiki\RemoteWiki;
+use stdClass;
+use User;
+use Wikimedia\Rdbms\DBConnRef;
 use Wikimedia\Rdbms\LBFactory;
 
 class ImportDumpRequestManager {
@@ -12,6 +17,9 @@ class ImportDumpRequestManager {
 	public const CONSTRUCTOR_OPTIONS = [
 		'ImportDumpCentralWiki',
 	];
+
+	/** @var DBConnRef */
+	private $dbr;
 
 	/** @var int */
 	private $ID;
@@ -24,6 +32,9 @@ class ImportDumpRequestManager {
 
 	/** @var ServiceOptions */
 	private $options;
+
+	/** @var stdClass|bool */
+	private $row;
 
 	/** @var UserFactory */
 	private $userFactory;
@@ -54,5 +65,102 @@ class ImportDumpRequestManager {
 	 */
 	public function fromID( int $requestID ) {
 		$this->ID = $requestID;
+
+		if ( $this->options->get( 'ImportDumpCentralWiki' ) ) {
+			$this->dbr = $this->lbFactory->getMainLB(
+				$this->options->get( 'ImportDumpCentralWiki' )
+			)->getConnectionRef( DB_REPLICA, [], $this->options->get( 'ImportDumpCentralWiki' ) );
+		} else {
+			$this->dbr = $this->lbFactory->getMainLB()->getConnectionRef( DB_REPLICA );
+		}
+
+		$this->row = $this->dbr->selectRow(
+			'importdump_requests',
+			'*',
+			[
+				'request_id' => $requestID,
+			],
+			__METHOD__
+		);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getComments(): array {
+		$row = $this->dbr->selectRow(
+			'importdump_requests',
+			'*',
+			[
+				'request_id' => $this->ID,
+			],
+			__METHOD__
+		);
+
+		if ( !$row ) {
+			return [];
+		}
+
+		$user = $this->userFactory->newFromActorId( $row->request_comment_actor );
+
+		return [
+			'comment' => $row->request_comment_text,
+			'timestamp' => $row->request_comment_timestamp,
+			'user' => $user,
+		];
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function exists(): bool {
+		return (bool)$this->row;
+	}
+
+	/**
+	 * @return User
+	 */
+	public function getRequester(): User {
+		return $this->userFactory->newFromActorId( $this->row->request_actor );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getReason(): string {
+		return $this->row->request_reason;
+	}
+
+	/**
+	 * @return ?string
+	 */
+	public function getSource(): ?string {
+		return $this->row->request_source;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTarget(): string {
+		return $this->row->request_target;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTimestamp(): string {
+		return $this->row->request_timestamp;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isPrivate(): bool {
+		if ( !ExtensionRegistry::getInstance()->isLoaded( 'CreateWiki' ) ) {
+			return false;
+		}
+
+		$remoteWiki = new RemoteWiki( $this->getTarget() );
+		return (bool)$remoteWiki->isPrivate();
 	}
 }
