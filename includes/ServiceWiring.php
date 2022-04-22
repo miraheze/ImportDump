4b7ec56037a571d53bebe1833e455b2f82ed3e3d
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-21 19:29:47 -0600

    Update ServiceWiring.php

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -7,6 +7,7 @@ use Miraheze\ImportDump\ImportDumpRequestManager;
 return [
 	'ImportDumpRequestManager' => static function ( MediaWikiServices $services ): ImportDumpRequestManager {
 		return new ImportDumpRequestManager(
+			$services->getConfigFactory()->makeConfig( 'ImportDump' ),
 			$services->getDBLoadBalancerFactory(),
 			new ServiceOptions(
 				ImportDumpRequestManager::CONSTRUCTOR_OPTIONS,
