8c20c7a03be0960cb29ab75d4d0c1a8cdb9658bf
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-25 10:58:35 -0600

    Update EchoRequestCommentPresentationModel.php

--- includes/Notifications/EchoRequestCommentPresentationModel.php
+++ includes/Notifications/EchoRequestCommentPresentationModel.php
@@ -4,6 +4,7 @@ namespace Miraheze\ImportDump\Notifications;
 
 use EchoDiscussionParser;
 use EchoEventPresentationModel;
+use Message;
 use RawMessage;
 
 class EchoRequestCommentPresentationModel extends EchoEventPresentationModel {
@@ -16,14 +17,14 @@ class EchoRequestCommentPresentationModel extends EchoEventPresentationModel {
 	}
 
 	/**
-	 * @return string
+	 * @return Message
 	 */
 	public function getHeaderMessage() {
 		return $this->msg( 'importdump-notification-header-comment' );
 	}
 
 	/**
-	 * @return string
+	 * @return Message
 	 */
 	public function getBodyMessage() {
 		$comment = $this->event->getExtraParam( 'comment' );
