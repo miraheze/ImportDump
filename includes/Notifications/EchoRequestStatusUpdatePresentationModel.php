19a7977eee8dbd415a5624c095fc3e85a7ddfb3a
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-25 10:44:08 -0600

    Update EchoRequestStatusUpdatePresentationModel.php

--- includes/Notifications/EchoRequestStatusUpdatePresentationModel.php
+++ includes/Notifications/EchoRequestStatusUpdatePresentationModel.php
@@ -7,14 +7,24 @@ use EchoEventPresentationModel;
 use RawMessage;
 
 class EchoRequestStatusUpdatePresentationModel extends EchoEventPresentationModel {
+
+	/**
+	 * @return string
+	 */
 	public function getIconType() {
 		return 'global';
 	}
 
+	/**
+	 * @return string
+	 */
 	public function getHeaderMessage() {
 		return $this->msg( 'importdump-notification-header-status-update' );
 	}
 
+	/**
+	 * @return string
+	 */
 	public function getBodyMessage() {
 		$comment = $this->event->getExtraParam( 'comment' );
 		$text = EchoDiscussionParser::getTextSnippet( $comment, $this->language );
@@ -22,10 +32,16 @@ class EchoRequestStatusUpdatePresentationModel extends EchoEventPresentationMode
 		return new RawMessage( '$1', [ $text ] );
 	}
 
+	/**
+	 * @return bool
+	 */
 	public function getPrimaryLink() {
 		return false;
 	}
 
+	/**
+	 * @return array
+	 */
 	public function getSecondaryLinks() {
 		$visitLink = [
 			'url' => $this->event->getExtraParam( 'request-url', 0 ),
