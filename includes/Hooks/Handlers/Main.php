25d51c238a1f5d661ff31d5fc1280db9e8bca3fb
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-26 10:47:05 -0600

    Update Main.php

--- includes/Hooks/Handlers/Main.php
+++ includes/Hooks/Handlers/Main.php
@@ -2,7 +2,10 @@
 
 namespace Miraheze\ImportDump\Hooks\Handlers;
 
+use EchoAttributeManager;
 use MediaWiki\User\Hook\UserGetReservedNamesHook;
+use Miraheze\ImportDump\Notifications\EchoRequestCommentPresentationModel;
+use Miraheze\ImportDump\Notifications\EchoRequestStatusUpdatePresentationModel;
 
 class Main implements UserGetReservedNamesHook {
 
@@ -12,5 +15,48 @@ class Main implements UserGetReservedNamesHook {
 	public function onUserGetReservedNames( &$reservedUsernames ) {
 		$reservedUsernames[] = 'ImportDump Extension';
 		$reservedUsernames[] = 'ImportDump Status Update';
+	}
+
+	/**
+	* @param array &$notifications
+	* @param array &$notificationCategories
+	* @param array &$icons
+	*/
+	public function onBeforeCreateEchoEvent(
+		&$notifications, &$notificationCategories, &$icons
+	) {
+		$notificationCategories['request-comment'] = [
+			'priority' => 3,
+			'tooltip' => 'importdump-echo-pref-tooltip-request-comment',
+		];
+
+		$notificationCategories['request-status-update'] = [
+			'priority' => 3,
+			'tooltip' => 'importdump-echo-pref-tooltip-request-status-update',
+		];
+
+		$notifications['request-comment'] = [
+			EchoAttributeManager::ATTR_LOCATORS => [
+				'EchoUserLocator::locateEventAgent'
+			],
+			'category' => 'request-comment',
+			'group' => 'positive',
+			'section' => 'alert',
+			'canNotifyAgent' => true,
+			'presentation-model' => EchoRequestCommentPresentationModel::class,
+			'immediate' => true,
+		];
+
+		$notifications['request-status-update'] = [
+			EchoAttributeManager::ATTR_LOCATORS => [
+				'EchoUserLocator::locateEventAgent'
+			],
+			'category' => 'request-status-update',
+			'group' => 'positive',
+			'section' => 'alert',
+			'canNotifyAgent' => true,
+			'presentation-model' => EchoRequestStatusUpdatePresentationModel::class,
+			'immediate' => true,
+		];
 	}
 }
