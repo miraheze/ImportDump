e3b41e95fa280d6211f9d5d47a8faacdffca2a9c
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-26 19:18:22 -0600

    Update Main.php

--- includes/Hooks/Handlers/Main.php
+++ includes/Hooks/Handlers/Main.php
@@ -4,6 +4,7 @@ namespace Miraheze\ImportDump\Hooks\Handlers;
 
 use EchoAttributeManager;
 use MediaWiki\User\Hook\UserGetReservedNamesHook;
+use Miraheze\ImportDump\Notifications\EchoNewRequestPresentationModel;
 use Miraheze\ImportDump\Notifications\EchoRequestCommentPresentationModel;
 use Miraheze\ImportDump\Notifications\EchoRequestStatusUpdatePresentationModel;
 
@@ -25,6 +26,11 @@ class Main implements UserGetReservedNamesHook {
 	public function onBeforeCreateEchoEvent(
 		&$notifications, &$notificationCategories, &$icons
 	) {
+		$notificationCategories['importdump-new-request'] = [
+			'priority' => 3,
+			'tooltip' => 'echo-pref-tooltip-importdump-new-request',
+		];
+
 		$notificationCategories['importdump-request-comment'] = [
 			'priority' => 3,
 			'tooltip' => 'echo-pref-tooltip-importdump-request-comment',
@@ -33,6 +39,18 @@ class Main implements UserGetReservedNamesHook {
 		$notificationCategories['importdump-request-status-update'] = [
 			'priority' => 3,
 			'tooltip' => 'echo-pref-tooltip-importdump-request-status-update',
+		];
+
+		$notifications['importdump-new-request'] = [
+			EchoAttributeManager::ATTR_LOCATORS => [
+				'EchoUserLocator::locateEventAgent'
+			],
+			'category' => 'importdump-new-request',
+			'group' => 'positive',
+			'section' => 'alert',
+			'canNotifyAgent' => true,
+			'presentation-model' => EchoNewRequestPresentationModel::class,
+			'immediate' => true,
 		];
 
 		$notifications['importdump-request-comment'] = [
