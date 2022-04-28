455b6fa0ae14d1e251390b31cd937ddbdb263edd
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-28 11:09:48 -0600

    Improve schema

--- sql/importdump_requests.sql
+++ sql/importdump_requests.sql
@@ -1,14 +1,16 @@
 CREATE TABLE /*_*/importdump_requests (
   request_id INT AUTO_INCREMENT NOT NULL,
   request_actor BIGINT UNSIGNED NOT NULL,
-  request_timestamp VARCHAR(32) NOT NULL,
+  request_timestamp BINARY(14) NOT NULL,
   request_source TEXT NOT NULL,
   request_target VARCHAR(64) NOT NULL,
   request_reason BLOB NOT NULL,
-  request_status VARCHAR(16) NOT NULL,
-  request_locked TINYINT UNSIGNED NOT NULL DEFAULT '0',
-  request_private TINYINT UNSIGNED NOT NULL DEFAULT '0',
+  request_status ENUM( 'complete', 'declined', 'inprogress', 'pending' ) NOT NULL,
+  request_locked TINYINT UNSIGNED DEFAULT 0 NOT NULL,
+  request_private TINYINT UNSIGNED DEFAULT 0 NOT NULL,
+  INDEX request_actor_timestamp (request_actor, request_timestamp),
+  INDEX request_timestamp (request_timestamp),
+  INDEX request_target (request_target),
+  INDEX request_status (request_status),
   PRIMARY KEY(request_id)
 ) /*$wgDBTableOptions*/;
-
-CREATE INDEX /*i*/request_id ON /*_*/importdump_requests (request_id);
