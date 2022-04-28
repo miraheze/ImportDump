b45fde1b5aef3f14be8747507e3716bdcda5a8bb
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-28 12:20:20 -0600

    Update importdump_request_comments.sql

--- sql/importdump_request_comments.sql
+++ sql/importdump_request_comments.sql
@@ -1,6 +1,10 @@
 CREATE TABLE /*_*/importdump_request_comments (
-  request_id INT NOT NULL,
+  request_id BIGINT UNSIGNED NOT NULL,
+  request_comment_id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
   request_comment_actor BIGINT UNSIGNED NOT NULL,
   request_comment_text BLOB NOT NULL,
-  request_comment_timestamp VARCHAR(32) NOT NULL
+  request_comment_timestamp BINARY(14) NOT NULL,
+  INDEX request_id (request_id),
+  INDEX request_comment_timestamp (request_comment_timestamp),
+  PRIMARY KEY(request_comment_id)
 ) /*$wgDBTableOptions*/;
