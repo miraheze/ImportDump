9772e97bb4aeb66d50902799978a9af7f38a12b8
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-08 13:06:10 -0600

    Enable remaining phan checks

--- .phan/config.php
+++ .phan/config.php
@@ -16,9 +16,6 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 	]
 );
 
-$cfg['suppress_issue_types'] = [
-	'PhanAccessMethodInternal',
-	'SecurityCheck-LikelyFalsePositive',
-];
+$cfg['suppress_issue_types'] = [];
 
 return $cfg;
