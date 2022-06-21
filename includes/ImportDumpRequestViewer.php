e89c1541a9c78d4a79df8a47a81626556ef272ec
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-06-21 14:12:06 -0600

    Update ImportDumpRequestViewer.php

--- includes/ImportDumpRequestViewer.php
+++ includes/ImportDumpRequestViewer.php
@@ -282,7 +282,7 @@ class ImportDumpRequestViewer {
 				$info .= Html::errorBox(
 					$this->context->msg( 'importdump-info-no-interwiki-prefix',
 						$this->importDumpRequestManager->getTarget(),
-						$this->importDumpRequestManager->getSource()
+						parse_url( $this->importDumpRequestManager->getSource(), PHP_URL_HOST )	
 					)->escaped()
 				);
 
