ca5507cbf46b8fdcc1a00d429b94fde722e03204
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-17 17:39:08 -0700

    Add EchoImportFailedPresentationModel

---
+++ includes/Notifications/EchoImportFailedPresentationModel.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Miraheze\ImportDump\Notifications;
+
+use EchoDiscussionParser;
+use EchoEventPresentationModel;
+use Message;
+
+class EchoImportFailedPresentationModel extends EchoEventPresentationModel {
+
+	/**
+	 * @return string
+	 */
+	public function getIconType() {
+		return 'global';
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getHeaderMessage() {
+		return $this->msg(
+			'importdump-notification-header-import-failed',
+			$this->event->getExtraParam( 'request-id' )
+		);
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getBodyMessage() {
+		$reason = EchoDiscussionParser::getTextSnippet(
+			$this->event->getExtraParam( 'reason' ),
+			$this->language
+		);
+
+		return $this->msg( 'importdump-notification-body-import-failed',
+			$reason
+		);
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function getPrimaryLink() {
+		return false;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getSecondaryLinks() {
+		$visitLink = [
+			'url' => $this->event->getExtraParam( 'request-url', 0 ),
+			'label' => $this->msg( 'importdump-notification-visit-request' )->text(),
+			'prioritized' => true,
+		];
+
+		return [ $visitLink ];
+	}
+}
