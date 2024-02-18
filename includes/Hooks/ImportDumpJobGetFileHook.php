0182667c6a1f892eee126df09bb67aad60d78b3c
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-18 11:47:03 -0700

    Pass ImportDumpRequestManager as well

--- includes/Hooks/ImportDumpJobGetFileHook.php
+++ includes/Hooks/ImportDumpJobGetFileHook.php
@@ -2,10 +2,13 @@
 
 namespace Miraheze\ImportDump\Hooks;
 
+use Miraheze\ImportDump\ImportDumpRequestManager;
+
 interface ImportDumpJobGetFileHook {
 	/**
 	 * @param string &$filePath
+	 * @param ImportDumpRequestManager $importDumpRequestManager
 	 * @return void
 	 */
-	public function onImportDumpJobGetFile( &$filePath ): void;
+	public function onImportDumpJobGetFile( &$filePath, $importDumpRequestManager ): void;
 }
