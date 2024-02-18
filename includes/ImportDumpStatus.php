31cca5000bbcdf403e3dc5608a98c5feab3ebf05
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-17 19:10:36 -0700

    Add ImportDumpStatus interface

---
+++ includes/ImportDumpStatus.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Miraheze\ImportDump;
+
+interface ImportDumpStatus {
+
+	public const STATUS_COMPLETE = 'complete';
+
+	public const STATUS_DECLINED = 'declined';
+
+	public const STATUS_FAILED = 'failed';
+
+	public const STATUS_INPROGRESS = 'inprogress';
+
+	public const STATUS_PENDING = 'pending';
+}
