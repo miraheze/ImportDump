89f820fc10769ba2baec204129c4cd9eea3035fa
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-05-09 13:06:43 -0600

    try

--- tests/phpunit/Specials/SpecialRequestImportTest.php
+++ tests/phpunit/Specials/SpecialRequestImportTest.php
@@ -105,11 +105,7 @@ class SpecialRequestImportTest extends SpecialPageTestBase {
 			$this->setSessionUser( $user, $user->getRequest() );
 		}
 
-		$request = new FauxRequest(
-			[], true,
-			RequestContext::getMain()->getRequest()->getSession()
-		);
-
+		$request = new FauxRequest( [], true );
 		$request->setUpload( 'wpUploadFile', [
 			'name' => basename( $formData['UploadFile'] ),
 			'type' => $extraData['mime-type'],
