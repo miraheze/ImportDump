754aa6ec0f8f8784a795f73bdcaaf242b359623e
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-08-19 20:13:46 -0600

    Rename configuration variables
    Next part of renaming the extension

--- includes/ConfigNames.php
+++ includes/ConfigNames.php
@@ -9,15 +9,15 @@ namespace Miraheze\ImportDump;
  */
 class ConfigNames {
 
-	public const EnableAutomatedJob = 'ImportDumpEnableAutomatedJob';
+	public const EnableAutomatedJob = 'RequestImportEnableAutomatedJob';
 
-	public const HelpUrl = 'ImportDumpHelpUrl';
+	public const HelpUrl = 'RequestImportHelpUrl';
 
-	public const InterwikiMap = 'ImportDumpInterwikiMap';
+	public const InterwikiMap = 'RequestImportInterwikiMap';
 
-	public const ScriptCommand = 'ImportDumpScriptCommand';
+	public const ScriptCommand = 'RequestImportScriptCommand';
 
-	public const UsersNotifiedOnAllRequests = 'ImportDumpUsersNotifiedOnAllRequests';
+	public const UsersNotifiedOnAllRequests = 'RequestImportUsersNotifiedOnAllRequests';
 
-	public const UsersNotifiedOnFailedImports = 'ImportDumpUsersNotifiedOnFailedImports';
+	public const UsersNotifiedOnFailedImports = 'RequestImportUsersNotifiedOnFailedImports';
 }
