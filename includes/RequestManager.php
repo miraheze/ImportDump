bc159cf4a53a0c46107f99d59bd26ff2b939ab34
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-08-19 20:27:18 -0600

    Rename

--- includes/RequestManager.php
+++ includes/RequestManager.php
@@ -301,7 +301,7 @@ class RequestManager {
 		return '';
 	}
 
-	public function getCommand(): string {
+	public function getImportDumpCommand(): string {
 		$command = $this->options->get( ConfigNames::ImportDumpScriptCommand );
 		if ( !$this->getInterwikiPrefix() ) {
 			$command = preg_replace( '/--username-prefix=?/', '', $command );
